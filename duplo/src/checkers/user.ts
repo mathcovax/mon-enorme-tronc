6ea2f84341782176cb6713d016b39df54979d9ee
mathcovax <campani.mathieu@gmail.com>
2024-04-06 15:43:39 +0000

    [feat] login back

---
+++ duplo/src/checkers/user.ts
@@ -0,0 +1,36 @@
+import { createTypeInput, GetTypeInput } from "@duplojs/type-input";
+import { Prisma } from "@prisma/client";
+
+export const inputUser = createTypeInput()
+	.add<"id", string>()
+	.add<"email", string>()
+	.build();
+
+export const userExistCheck = duplo
+	.createChecker("userExist")
+	.handler(async ({name, value}: GetTypeInput<typeof inputUser>, output) => {
+		let where: Prisma.UserFindFirstArgs["where"];
+		
+		if(name === "id"){
+			where = {
+				id: value
+			};
+		}
+		else if(name === "email"){
+			where = {
+				email: value
+			};
+		}
+
+		const user = await prisma.user.findFirst({
+			where,
+		});
+
+		if(user){
+			return output("user.exist", user);
+		}
+		else {
+			return output("user.notfound", null);
+		}
+	})
+	.build();
--- duplo/src/routes/login.ts
+++ duplo/src/routes/login.ts
@@ -1,3 +1,6 @@
+import { firebaseTokenCheck } from "@checkers/token";
+import { inputUser, userExistCheck } from "@checkers/user";
+import { AccessToken } from "@services/token";
 
 /* METHOD : POST, PATH : /login */
 export const POST = (method: Methods, path: string) => duplo
@@ -5,9 +8,51 @@ export const POST = (method: Methods, path: string) => duplo
 	.extract({
 		body: zod.string()
 	})
+	.check(
+		firebaseTokenCheck,
+		{
+			input: p => p("body"),
+			result: "firebase.token.valide",
+			catch: (res, info) => {
+				throw new UnauthorizedHttpException(info);
+			},
+			indexing: "decodedIdToken",
+		},
+		new IHaveSentThis(UnauthorizedHttpException.code, "firebase.token.invalide")
+	)
+	.check(
+		userExistCheck,
+		{
+			input: p => inputUser.email(
+				p("decodedIdToken").email
+			),
+			catch: () => undefined,
+			indexing: "user"
+		}
+	)
+	.cut(
+		async ({pickup}) => {
+			const user = pickup("user");
+			
+			if(!user){
+				const email = pickup("decodedIdToken").email;
+
+				const user = await prisma.user.create({
+					data: {email}
+				});
+				
+				return {
+					user
+				};
+			}
+		},
+		["user"]
+	)
 	.handler(
 		({pickup}) => {
-			throw new OkHttpException("user.login", "my_super_token");
+			const {id, email} = pickup("user");
+			const accessToken = AccessToken.generate({id, email});
+			throw new OkHttpException("user.login", accessToken);
 		},
-		new IHaveSentThis(OkHttpException.code, "user.login", zod.number())
+		new IHaveSentThis(OkHttpException.code, "user.login", zod.string())
 	);
