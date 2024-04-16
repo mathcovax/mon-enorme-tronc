59e31c6c2f1ac7cc0e979a30b7b02ded8f9f88af
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2024-04-16 13:41:48 +0000

    feat(23): update toasts position

--- vue/src/components/ui/toast/ToastViewport.vue
+++ vue/src/components/ui/toast/ToastViewport.vue
@@ -17,6 +17,6 @@ const delegatedProps = computed(() => {
 <template>
 	<ToastViewport
 		v-bind="delegatedProps"
-		:class="cn('fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]', props.class)"
+		:class="cn('fixed bottom-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]', props.class)"
 	/>
 </template>
