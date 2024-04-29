2f34a6eed5712c77df2f7cd1ab81c8e008c89de2
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2024-04-29 15:02:06 +0000

    feat(23): clean

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,29 +1,6 @@
 <script setup lang="ts">
 import TheToaster from "@/components/ui/toast/TheToaster.vue";
 
-const toasts = ref([]);
-
-const showToast = () => {
-	toasts.value.forEach((toast) => {
-		let toastFunction;
-
-		if ((toast as { type: string }).type === "success") {
-			toastFunction = useSuccessToast();
-		} else if ((toast as { type: string }).type === "info") {
-			toastFunction = useDefaultToast();
-		} else if ((toast as { type: string }).type === "error") {
-			toastFunction = useErrorToast();
-		} else {
-			toastFunction = useDefaultToast();
-		}
-
-		toastFunction((toast as { description: string }).description);
-	});
-};
-
-onMounted(() => {
-	watch(() => toasts.value, showToast, { immediate: true });
-});
 </script>
 
 <template>
