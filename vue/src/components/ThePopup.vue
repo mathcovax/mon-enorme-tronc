3945bc5b96ae65a2e4e4ba87347ef79a33fcdfa7
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2024-05-14 15:22:10 +0000

    feat(55): improve popup

--- vue/src/components/ThePopup.vue
+++ vue/src/components/ThePopup.vue
@@ -19,6 +19,16 @@ const open = () => isOpen.value = true;
 const close = () => isOpen.value = false;
 
 defineExpose({ isOpen, open, close });
+
+onMounted(() => {
+	watch(isOpen, (value) => {
+		if (value) {
+			document.body.classList.add("overflow-hidden");
+		} else {
+			document.body.classList.remove("overflow-hidden");
+		}
+	}, { immediate: true });
+});
 </script>
 
 <template>
@@ -30,10 +40,17 @@ defineExpose({ isOpen, open, close });
 	<Teleport to="#popup">
 		<div 
 			v-if="isOpen" 
-			class="fixed w-full h-full top-0 left-0 inset-0 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 flex items-center justify-center"
+			class="fixed w-full h-full top-0 left-0 inset-0 bg-black/80 data-[state=open]:overflow-hidden  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 flex items-center justify-center"
 			:style="{ zIndex: priority }"
 		>
-			<div class="bg-white p-5 rounded-lg">
+			<div class="relative bg-white p-6 rounded-lg">
+				<TheIcon	
+					icon="close"
+					size="2xl"
+					class="absolute top-1 right-1 cursor-pointer"
+					@click="close"
+				/>
+
 				<slot 
 					name="popupContent" 
 					:close="close" 
