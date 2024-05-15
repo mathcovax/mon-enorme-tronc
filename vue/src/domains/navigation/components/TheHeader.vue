fc8f8599c99f3f38c67b3f073811a414f63a2c37
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2024-05-15 09:29:30 +0000

    fix(hf): improve home navbar

--- vue/src/domains/navigation/components/TheHeader.vue
+++ vue/src/domains/navigation/components/TheHeader.vue
@@ -1,27 +1,70 @@
 <script setup lang="ts">
-const displayNav = ref(false);
-
-function handleBurgerButtonToggle(value: boolean) {
-	displayNav.value = value;
-}
+import { 
+	CircleUser,
+	Menu,
+	Home,
+	Package,
+	ShoppingCart,
+} from "lucide-vue-next";
 </script>
 
 <template>
-	<header class="sticky top-0 z-10 h-24 p-6 flex items-center bg-white">
-		<div class="container flex gap-10 justify-between lg:justify-center items-center">
-			<div class="flex gap-3 items-center">
-				<BurgerButton @on-toggle="handleBurgerButtonToggle" />
-
-				<RouterLink
-					to="/"
-					class="text-2xl font-bold"
+	<header class="sticky top-0 z-10 h-24 flex items-center gap-4 bg-white px-4 md:px-6">
+		<TheSheet>
+			<SheetTrigger as-child>
+				<TheButton
+					variant="outline"
+					size="icon"
+					class="shrink-0 md:hidden"
 				>
-					MET
-				</RouterLink>
-			</div>
+					<Menu class="h-5 w-5" />
 
+					<span class="sr-only">Toggle navigation menu</span>
+				</TheButton>
+			</SheetTrigger>
+
+			<SheetContent
+				side="left"
+				class="flex flex-col"
+			>
+				<nav class="grid gap-2 text-lg font-medium">
+					<RouterLink
+						to="/"
+						class="text-center text-2xl font-bold"
+					>
+						<span>MET</span>
+					</RouterLink>
+
+					<RouterLink
+						to="#"
+						class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+					>
+						<Home class="h-5 w-5" />
+						Meilleures ventes
+					</RouterLink>
+
+					<RouterLink
+						to="#"
+						class="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+					>
+						<ShoppingCart class="h-5 w-5" />
+						Nouveautés
+					</RouterLink>
+
+					<RouterLink
+						to="#"
+						class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+					>
+						<Package class="h-5 w-5" />
+						Produits
+					</RouterLink>
+				</nav>
+			</SheetContent>
+		</TheSheet>
+
+		<div class="w-full flex-1 flex gap-10 justify-between md:justify-center items-center">
 			<nav>
-				<ul class="hidden lg:flex gap-6">
+				<ul class="hidden md:flex gap-6">
 					<li>
 						<RouterLink to="#">
 							Meilleures ventes
@@ -42,7 +85,7 @@ function handleBurgerButtonToggle(value: boolean) {
 				</ul>
 			</nav>
 
-			<div class="hidden lg:block grow max-w-144">
+			<div class="hidden md:block grow max-w-144">
 				<input
 					type="text"
 					placeholder="Rechercher un produit..."
@@ -51,7 +94,7 @@ function handleBurgerButtonToggle(value: boolean) {
 			</div>
 
 			<div class="flex gap-3 items-center">
-				<button class="lg:hidden">
+				<button class="md:hidden">
 					<TheIcon
 						icon="magnify"
 						size="2xl"
@@ -65,38 +108,34 @@ function handleBurgerButtonToggle(value: boolean) {
 					/>
 				</RouterLink>
 
-				<RouterLink to="/account">
-					<TheIcon
-						icon="account-circle-outline"
-						size="2xl"
-					/>
-				</RouterLink>
+				<DropdownMenu>
+					<DropdownMenuTrigger as-child>
+						<TheButton
+							variant="secondary"
+							size="icon"
+							class="rounded-full"
+						>
+							<CircleUser class="h-5 w-5" />
+
+							<span class="sr-only">Toggle user menu</span>
+						</TheButton>
+					</DropdownMenuTrigger>
+
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+
+						<DropdownMenuSeparator />
+
+						<DropdownMenuItem>Settings</DropdownMenuItem>
+
+						<DropdownMenuItem>Support</DropdownMenuItem>
+
+						<DropdownMenuSeparator />
+
+						<DropdownMenuItem>Logout</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
 			</div>
 		</div>
 	</header>
-
-	<nav
-		class="fixed z-40 w-72 h-[calc(100vh-6rem)] bg-white transition-transform duration-300"
-		:class="{ '-translate-x-72': !displayNav }"
-	>
-		<ul class="flex flex-col items-center gap-6 p-6">
-			<li>
-				<RouterLink to="#">
-					Meilleures ventes
-				</RouterLink>
-			</li>
-
-			<li>
-				<RouterLink to="#">
-					Nouveautés
-				</RouterLink>
-			</li>
-
-			<li>
-				<RouterLink to="#">
-					Produits
-				</RouterLink>
-			</li>
-		</ul>
-	</nav>
 </template>
