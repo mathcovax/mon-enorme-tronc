03b339878cc6e537b58f8427039d21a89a2ac48b
zeriix <wflorentin@myges.fr>
2024-05-18 18:49:43 +0200

    feat(53): init router

--- vue/src/domains/organization/router.ts
+++ vue/src/domains/organization/router.ts
@@ -1,7 +1,11 @@
 import type { RouteRecordRaw } from "vue-router";
 
-export const routerPageNameOrganization = Object.freeze({
+export const routerPageOrganization = Object.freeze({
 	ORGANIZATION_MANAGE_USER: "organization-manage-user",
+	MANAGE_PRODUCT_SHEET: "manage-product-sheet",
+	CREATE_PRODUCT_SHEET: "create-product-sheet",
+	EDIT_PRODUCT_SHEET: "edit-product-sheet",
+	DASHBOARD: "organization"
 });
 
 export default (): RouteRecordRaw[] => [
@@ -10,15 +14,30 @@ export default (): RouteRecordRaw[] => [
 		component: () => import("./layout/OrganizationLayout.vue"),
 		children: [
 			{
-				name: "organization",
+				name: routerPageOrganization.DASHBOARD,
 				path: "/organization/:organizationId/dashboard",
 				component: () => import("./pages/OrganizationPage.vue"),
 			},
 			{
-				name: routerPageNameOrganization.ORGANIZATION_MANAGE_USER,
+				name: routerPageOrganization.ORGANIZATION_MANAGE_USER,
 				path: "/organization/:organizationId/manage-user",
 				component: () => import("./pages/ManageUserOrganization.vue"),
 			},
-		],
-	},
+			{
+				name: routerPageOrganization.MANAGE_PRODUCT_SHEET,
+				path: "/organization/:organizationId/manage-product-sheet",
+				component: () => import("./pages/ManageProductSheet.vue")
+			},
+			{
+				name: routerPageOrganization.CREATE_PRODUCT_SHEET,
+				path: "/organization/:organizationId/create-product-sheet",
+				component: () => import("./pages/FormProductSheet.vue")
+			},
+			{
+				name: routerPageOrganization.EDIT_PRODUCT_SHEET,
+				path: "/cate organization/:organizationId/edit-product-sheet/:productSheetId",
+				component: () => import("./pages/FormProductSheet.vue")
+			}
+		]
+	}
 ];
