c5f0ac10c99e3a7b6c5028e18facc4555e0d4e86
Vitalx <liamdu92@gmail.com>
2024-05-18 17:57:30 +0200

    feat(53): add method get for product sheets with name and page params and unit test

---
+++ duplo/src/routes/organization/{organizationId}/product-sheets.test.ts
@@ -0,0 +1,44 @@
+import { MockPrisma } from "@test/mocks/providers";
+import { duploTesting } from "@test/setup";
+import { GET } from "./product-sheets";
+
+describe("GET /organization/{organizationId}/product-sheets", () => {
+	beforeEach(() => {
+		MockPrisma.resest();
+	});
+
+	it("get product-sheets", async () => {
+		const spy = vi.fn(() => []);
+		MockPrisma.set("product_sheet", "findMany", spy);
+
+		const res = await duploTesting
+			.testRoute(GET("GET", ""))
+			.setRequestProperties({
+				params: {
+					organizationId: "1",
+				},
+				query: {
+					name: "eeee",
+				}
+			})
+			.mockChecker(
+				"organizationExist",
+				{ info: "organization.exist", data: { id: "1234" } },
+			)
+			.launch();
+
+		expect(spy).lastCalledWith({
+			where: {
+				organizationId: "1",
+				name: {
+					contains: "eeee",
+					mode: "insensitive",
+				}
+			},
+			skip: 0,
+			take: 10,
+		});
+		console.log(res.body);
+		expect(res.information).toBe("product_sheets.found");
+	});
+});
---
+++ duplo/src/routes/organization/{organizationId}/product-sheets.ts
@@ -0,0 +1,52 @@
+import { inputOrganization, organizationExistCheck } from "@checkers/organization";
+import { productSheetSchema } from "@schemas/product_sheet";
+import { mustBeConnected } from "@security/mustBeConnected";
+
+/* METHOD : GET, PATH : /organization/{organizationId}/product-sheets */
+export const GET = (method: Methods, path: string) =>
+	mustBeConnected({ pickup: ["accessTokenContent"] })
+		.declareRoute(method, path)
+		.extract({
+			params: {
+				organizationId: zod.string()
+			},
+			query: {
+				page: zod.coerce.number().default(0),
+				name: zod.string().optional()
+			}
+		})
+		.check(
+			organizationExistCheck,
+			{
+				input: p => inputOrganization.id(
+					p("organizationId")
+				),
+				...organizationExistCheck.preCompletions.wantExist,
+				indexing: undefined
+			},
+			new IHaveSentThis(NotFoundHttpException.code, "organization.notfound")
+		)
+		.handler(
+			async ({ pickup }) => {
+				const organizationId = pickup("organizationId");
+				const page = pickup("page");
+				const name = pickup("name");
+
+				const productSheets = await prisma.product_sheet.findMany({
+					where: {
+						organizationId: organizationId,
+						name: name
+							? {
+								contains: name,
+								mode: "insensitive"
+							}
+							: undefined
+					},
+					take: 10,
+					skip: page * 10
+				});
+
+				throw new OkHttpException("product_sheets.found", productSheets);
+			},
+			new IHaveSentThis(OkHttpException.code, "product_sheets.found", productSheetSchema.array())
+		);
