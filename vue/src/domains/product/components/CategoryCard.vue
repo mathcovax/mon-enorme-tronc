f8d3dd6646d87399d071ec4f7e0a83ecc87de3dd
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2024-06-05 15:13:32 +0000

    feat(92): add categories page

---
+++ vue/src/domains/product/components/CategoryCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { Category } from "@/lib/utils";
+
+const { CATEGORY_PAGE } = routerPageName;
+
+defineProps<{
+	category: Category;
+}>();
+</script>
+
+<template>
+	<TheCard class="border-0 rounded-md bg-gradient-to-b from-muted/50 to-muted">
+		<RouterLink :to="{ name: CATEGORY_PAGE, params: { categoryName: category.name } }">
+			<CardHeader>
+				<img
+					v-if="category.imageUrl"
+					:src="category.imageUrl "
+					alt="placeholder"
+					class="w-full aspect-portrait rounded-2xl"
+				>
+
+				<div
+					v-else
+					class="w-full aspect-portrait flex items-center justify-center rounded-2xl"
+				>
+					<TheIcon
+						icon="image-outline"
+						size="3xl"
+						class="text-muted-foreground"
+					/>
+				</div>
+			</CardHeader>
+
+			<CardContent>
+				<CardTitle class="mb-3">
+					{{ category.name }}
+				</CardTitle>
+			</CardContent>
+		</RouterLink>
+	</TheCard>
+</template>
--- vue/src/domains/product/router.ts
+++ vue/src/domains/product/router.ts
@@ -1,11 +1,17 @@
 import type { RouteRecordRaw } from "vue-router";
 
 export const routerPageNameCategory = Object.freeze({
+	CATEGORIES_PAGE: "categories",
 	CATEGORY_PAGE: "category",
 	PRODUCT_PAGE: "product",
 });
 
 export default (): RouteRecordRaw[] => [
+	{
+		name: routerPageNameCategory.CATEGORIES_PAGE,
+		path: "/categories",
+		component: () => import("./pages/CategoriesPage.vue"),
+	},
 	{
 		name: routerPageNameCategory.CATEGORY_PAGE,
 		path: "/category/:categoryName",
